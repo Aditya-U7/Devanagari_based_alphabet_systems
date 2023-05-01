6d7e82d367f2064c3ed334a07e29f432b8806e5c
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-05-01 16:51:02 +0530

    Add files via upload

--- hindi_varnamala.py
+++ hindi_varnamala.py
@@ -1,28 +1,41 @@
+def print_vowels():
+
+    for i in range(5, 21):                    # This for loop for the vowels. 
+        
+        if i in [12, 13, 14, 17, 18]:
+            continue
+        
+        print(chr(0x0900 + int(hex(i), 16)), " ", end="")
+
+    print(chr(0x0902), chr(0x0905), sep="", end=" ")
+    print(chr(0x0905), chr(0x0903), sep="")
 
 
-print()
-for i in range(5,21):
-  if i in [12,13,14,17,18]:
-   continue
-  print(chr(0x0900+int(hex(i),16)),' ',end='')
+def print_consonants():
+    
+    for i in range(5, 42):                   # This one is for consonants.
+        if i in [25, 33, 35, 36]:
+            continue
 
-print(chr(0x0902),chr(0x0905),sep='',end=' ')
-print(chr(0x0905),chr(0x0903),sep='')
+        j = int(hex(i), 16)
+        x = chr(0x0910 + j)
 
-print()
+        print(x," ", end="",)
 
-for i in range(5,42):
-  if i in [25,33,35,36]:
-   continue
-  
-  j = int(hex(i),16)
-  x = chr(0x0910+j)
-  
-  print(x,' ',end='',)
-  
-  if ( ( i + 1 ) % 5 == 0 ) and (i < 29) or ( i == 30 ) or ( i == 37 ):
-   print()
-  
-      
-print()
+        if (i + 1) % 5 == 0 and i < 29 or i == 30 or i == 37:
+            print()
+
+
+def print_Hindi_varnamala():
+   
+    print_vowels()
+    
+    print()
+    
+    print_consonants()
+    
+    print()
+    
+    
+print_Hindi_varnamala()
   
