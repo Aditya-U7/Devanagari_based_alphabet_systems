b28e092a48bf8641094cf58f02688960d145b19c
Aditya-U7 <66748266+Aditya-U7@users.noreply.github.com>
2023-05-28 03:08:23 +0530

    Add files via upload

--- hindi_varnamala.py
+++ hindi_varnamala.py
@@ -1,32 +1,33 @@
 def print_vowels():
 
-    for i in range(5, 21):                    # This for loop for the vowels. 
+    for i in range(5, 21):                    
         
         if i in [12, 13, 14, 17, 18]:
             continue
         
-        print(chr(0x0900 + int(hex(i), 16)), " ", end="")
-
+        print(chr(0x0900 + i), " ", end="")
+     
     print(chr(0x0902), chr(0x0905), sep="", end=" ")
-    print(chr(0x0905), chr(0x0903), sep="")
-
+    print(chr(0x0905), chr(0x0903), sep="")    
+        
 
 def print_consonants():
     
-    for i in range(5, 42):                   # This one is for the consonants.
+    for i in range(5, 42):                   
+        
         if i in [25, 33, 35, 36]:
             continue
 
-        j = int(hex(i), 16)
-        x = chr(0x0910 + j)
+        x = chr(0x0910 + i)
 
         print(x," ", end="")
 
-        if (i + 1) % 5 == 0 and i < 29 or i == 30 or i == 37:
-            print()
+        if ((i+1) % 5 == 0 and i < 29) or (i == 30) or (i == 37):
+            
+            print()        
 
 
-def print_Hindi_varnamala():
+def print_hindi_varnamala():
    
     print_vowels()
     
@@ -35,7 +36,8 @@ def print_Hindi_varnamala():
     print_consonants()
     
     print()
-    
-    
-print_Hindi_varnamala()
+
+#####################
+
+print_hindi_varnamala()
   
